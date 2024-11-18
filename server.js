2f966824562734fca0064c83fd1e2d6001610435
melih <durmazmelih.ml@gmail.com>
2024-11-18 15:21:58 +0300

    Update Content-Security-Policy header to use the new domain for frame-ancestors

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 
 app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "frame-ancestors 'self' https://t.me/SurvialGameExportBot/SurvivalGame");
+    res.setHeader('Content-Security-Policy', "frame-ancestors 'self' https://survivalgame.onrender.com");
     next();
 });
 
