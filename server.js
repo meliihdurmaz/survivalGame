41c4cd861ca4af835d5c03991de75241dae61ff3
melih <durmazmelih.ml@gmail.com>
2024-11-18 15:12:08 +0300

    Remove Cross-Origin-Embedder-Policy header from server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(express.static(path.join(__dirname, '.')));
 
 app.use((req, res, next) => {
     res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
     next();
