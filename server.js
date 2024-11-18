e40377cbda39e54a106001b9c343a47590236190
melih <durmazmelih.ml@gmail.com>
2024-11-18 15:19:31 +0300

    Add Cross-Origin-Embedder-Policy and Content-Security-Policy headers in server.js

--- server.js
+++ server.js
@@ -9,11 +9,21 @@ app.use(express.static(path.join(__dirname, '.')));
 
 app.use((req, res, next) => {
     res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
+
+    // res.setHeader('X-Frame-Options', 'ALLOW-FROM https://t.me/SurvialGameExportBot/SurvivalGame');
+
     next();
 });
 
+
+
+app.use((req, res, next) => {
+    res.setHeader('Content-Security-Policy', "frame-ancestors 'self' https://t.me/SurvialGameExportBot/SurvivalGame");
+    next();
+});
 
 
 
