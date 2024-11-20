906b61d5f0bc0b127bb5fc90a1310e7583043879
melih <durmazmelih.ml@gmail.com>
2024-11-20 17:49:39 +0300

    Initialize Express server and set up static file serving; add Procfile and package.json

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+// Cross-Origin Isolation başlıkları ekleyin
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    next();
+});
+
+
+app.use(express.static(path.join(__dirname, ".")));
+
+// Serve the index file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, ".", "index.html"));
+});
+
+
+// Sunucuyu başlatın
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+  
