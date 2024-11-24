8114ddcc0bddd1bc10ba7b7c2d0ad4a75edb652d
melih <durmazmelih.ml@gmail.com>
2024-11-24 14:18:54 +0300

    Enhance CORS handling in app.js and coi-serviceworker.min.js by adding Access-Control-Allow-Origin and Access-Control-Allow-Headers headers

--- app.js
+++ app.js
@@ -6,20 +6,16 @@ const PORT = process.env.PORT || 3000;
 
 // Cross-Origin Isolation başlıkları ekleyin
 app.use((req, res, next) => {
-    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
     next();
 });
 
 // Tüm statik dosyalara Cross-Origin Isolation başlıkları ekleyin
 app.use(express.static(path.join(__dirname, ".")));
 
-// Serve the index file
-app.get("/", (req, res) => {
-    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-    res.sendFile(path.join(__dirname, ".", "index.html"));
-});
 
 // Sunucuyu başlatın
 app.listen(PORT, () => {
